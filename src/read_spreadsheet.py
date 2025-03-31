8217bb4bb116bd396840acc9dd6454cfdab4bc86
Yuri Amaral Santos Rocha <yuriamaral.yas@gmail.com>
2025-03-30 22:47:06 -0300

    refactor: Improved file reading, exception handling and prevention of copy warnings

--- src/read_spreadsheet.py
+++ src/read_spreadsheet.py
@@ -3,21 +3,31 @@ import glob
 
 def get_file():
     list_of_files = glob.glob('data/*.xlsx')
+    if not list_of_files:
+        print("No Excel files found in the 'data' directory.")
+        return None
+    
     file_path = list_of_files[0]
     return file_path
 
 FILE_PATH = get_file()
 
 def read_spreasheet(file_path):
+    if file_path is None:
+        return None
     try:
         df = pd.read_excel(file_path)
         return df
     except FileNotFoundError:
         print("File not found. Please check the file path.")
+    except ValueError as e:
+        print(f"Error reading the Excel file: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
     return None
 
 def group_packs_by_address(df):
-    df_subset = df.loc[:, ['Sequence', 'Latitude', 'Longitude']]
+    df_subset = df.loc[:, ['Sequence', 'Latitude', 'Longitude']].copy()
     df_subset['Sequence'] = df_subset['Sequence'].astype(str)
     
     grouped = df_subset.groupby(['Latitude', 'Longitude'], as_index=False).agg({
