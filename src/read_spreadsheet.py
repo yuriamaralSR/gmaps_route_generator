7575df54163dda1a164949c741ed6370d805d4ae
Yuri Amaral Santos Rocha <yuriamaral.yas@gmail.com>
2025-03-30 22:26:26 -0300

    refactor: Add exception handling to spreadsheet reading function

--- src/read_spreadsheet.py
+++ src/read_spreadsheet.py
@@ -1,4 +1,17 @@
 import pandas as pd
+import glob 
 
-df = pd.read_excel('data/B-3.xlsx')
+def get_file():
+    list_of_files = glob.glob('data/*.xlsx')
+    file_path = list_of_files[0]
+    return file_path
 
+FILE_PATH = get_file()
+
+def read_spreasheet(file_path):
+    try:
+        df = pd.read_excel(file_path)
+        return df
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+    return None
